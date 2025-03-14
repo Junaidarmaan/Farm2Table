c48ca7bc4d9209bba5a5a38cd3e7753bbd7d7584
junaid <junnubest@gmail.com>
2025-03-14 21:20:57 +0530

    added web files to static

--- src/main/java/com/junaid/farm2table/Configs/WebSocketConfig.java
+++ src/main/java/com/junaid/farm2table/Configs/WebSocketConfig.java
@@ -2,14 +2,14 @@ package com.junaid.farm2table.Configs;
 
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import com.junaid.farm2table.WebSocket.CustomWebSocketHandler;;
+import com.junaid.farm2table.WebSocket.CustomWebSocketHandler;
 
 public class  WebSocketConfig implements WebSocketConfigurer{
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         
-        registry.addHandler(new CustomWebSocketHandler(), "/chat").setAllowedOriginPatterns("*");
+        registry.addHandler(new CustomWebSocketHandler(), "/chat").setAllowedOrigins("*");
     }
 
 }
